879e37f76cdda25d7de988f35d8e6c634d19e012
lbacon17 <louis.bacon17@icloud.com>
2020-12-31 14:18:19 +0000

    created login, logout and register functions and conditional rendering on homepage

--- app.py
+++ app.py
@@ -25,6 +25,70 @@ def home_page():
     return render_template("index.html", expenses=expenses)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+        existing_email = mongo.db.users.find_one({"email_address": request.form.get("email").lower()})
+
+        # check whether user already exists
+        if existing_user:
+            flash("User already exists")
+            return redirect(url_for("register"))
+        
+        # check whether e-mail address was already used to sign up
+        if existing_email:
+            flash("An account already exists for this e-mail address!")
+            return redirect(url_for("register"))
+        
+        create_account = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "email_address": request.form.get("email").lower()
+        }
+        mongo.db.users.insert_one(create_account)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
+        return redirect(url_for("home_page", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})        
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    return redirect(url_for(
+                        "home_page", username=session["user"]))
+
+            else:
+                # invalid password match
+                flash("Invalid login credentials!")
+                return redirect(url_for("login"))
+
+        else: 
+            # username doesn't exist
+            flash("Invalid login credentials!")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")    
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("home_page"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
     port=int(os.environ.get("PORT")), 
